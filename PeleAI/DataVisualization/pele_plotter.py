881b1681ca787845c2196a6c81eca1af87d06cde
AlexisMolinaMR <alexis.molina@alum.esci.upf.edu>
2020-01-13 15:25:08 +0100

    Functional script

--- PeleAI/DataVisualization/pele_plotter.py
+++ PeleAI/DataVisualization/pele_plotter.py
@@ -1,24 +1,21 @@
 import pandas as pd
 import glob
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
+import matplotlib.pyplot as plt # to seaborn
 import os
 import sys
 
+# to argparse
 
 path = sys.argv[1] # path by sys argv for now
 metric = sys.argv[2] # plot metric vs B.E.
 show = sys.argv[3] # show if set to 'show', save if set to 'save'
-#save_path = sys.argv[4] # path to save the plots
+save_path = sys.argv[4] # path to save the plots
 
-fields = ['hbond_H_val_690', 'RMSD_ligand', 'docking_com_dist', 'Binding Energy'] # to be given as input
+fields = ['hbond_H_val_690', 'RMSD_ligand', 'docking_com_dist', 'Binding Energy', 'trajectory'] # to be given as input (argparse)
 
-for filename in glob.glob(os.path.join(path, 'summary.csv')):
+for filename in glob.glob(os.path.join(path, '*.csv')):
     pele_out_summary = pd.read_csv(filename, sep=';', usecols = fields)
     pele_out_summary.rename({'Binding Energy': 'Binding_Energy'}, axis = 'columns', inplace = True)
-
-    foldername = os.path.basename(path)
 
     if metric == "hbond":
         plt.scatter(pele_out_summary.hbond_H_val_690, pele_out_summary.Binding_Energy)
@@ -31,9 +28,11 @@ for filename in glob.glob(os.path.join(path, 'summary.csv')):
         plt.xlabel("Docking distance")
 
     plt.ylabel("Binding Energy")
-    plt.title(foldername)
+    plt.title(pele_out_summary.trajectory[0].split('/')[0])
 
     if show == 'show':
         plt.show()
     else:
-        plt.savefig("pele_plot.png")
+        plt.savefig(save_path + '/' + pele_out_summary.trajectory[0].split('/')[0] + '_' + metric + "_pele_plot.png")
+
+    plt.close()
