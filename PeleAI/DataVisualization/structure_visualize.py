ace2af9a42b7970ed4983dfec5ab25cd61ee2ea5
AlexisMolinaMR <alexis.molina@alum.esci.upf.edu>
2020-01-15 12:38:51 +0100

    2D visualizer for ligands using rdkit

--- PeleAI/DataVisualization/structure_visualize.py
+++ PeleAI/DataVisualization/structure_visualize.py
@@ -23,7 +23,7 @@ from rdkit.Chem import Draw
 path = sys.argv[1] # path where PDB's are stored
 save_path = sys.argv[2] # save png's with 2d ligand structure
 ligand_pdb = sys.argv[3] # ligand name as in pdb file ## extend to set of ligands
-#keep_ligand = sys.argv[4] # yes/no ## remove or save ligands
+keep_ligand = sys.argv[4] # yes/no ## remove or save ligands
 
 for filename in glob.glob(os.path.join(path, '*.pdb')):
     ligand_name = (os.path.basename(filename).split("_trajectory")[0])
@@ -39,3 +39,8 @@ for filename in glob.glob(os.path.join(path, '*.pdb')):
 
     ligand = AllChem.MolFromPDBFile(save_path + ligand_name + ".pdb")
     Draw.MolToFile(ligand, save_path + ligand_name + '.png')
+
+    if keep_ligand == "yes":
+        continue
+    else:
+        os.remove(save_path + ligand_name + ".pdb")
