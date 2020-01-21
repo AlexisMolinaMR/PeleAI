0a9f73af54d4effd4bba84f99c72098f7dd869d6
AlexisMolinaMR <alexis.molina@alum.esci.upf.edu>
2020-01-21 17:33:26 +0100

    Descriptors to excel

--- PeleAI/descriptors/descriptor_calculator.py
+++ PeleAI/descriptors/descriptor_calculator.py
@@ -5,20 +5,39 @@ import pandas as pd
 import warnings
 import glob
 import os
+import time
+
+start_time = time.time()
 
 calc = Calculator(descriptors)
 
-#print(len(calc.descriptors))
-
-#mols = [Chemfor filename in glob.glob(os.path.join(path, '*.sdf')):
-
 path = "/home/alexis/Desktop/PeleAI_data/simulation_pdb/ligand_extract/ligands_2D/"
 
-descriptors_df = pd.DataFrame()
-
 for filename in glob.glob(os.path.join(path, '*.sdf')):
+
     suppl = ForwardSDMolSupplier(filename)
+
+    print("Calculating decriptors for " + os.path.basename(filename))
     df = calc.pandas(suppl)
-    descriptors_df.append(df)
-#    for mol in suppl:
-#        print(list(calc(mol))[0])
+
+    if os.path.exists('dummy.xlsx'):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+
+    with pd.ExcelWriter('dummy.xlsx', mode = append_write, engine="openpyxl") as writer:
+        print("Writing descriptors")
+        df.to_excel(writer)
+
+writer.save()
+
+descriptor_df = pd.read_excel('dummy.xlsx', None)
+all_ligand_descriptors = []
+for key in descriptor_df.keys():
+    all_ligand_descriptors.append(descriptor_df[key])
+descriptors_concatenated = pd.concat(all_ligand_descriptors,axis=0,ignore_index=True)
+writer = pd.ExcelWriter('final_descriptors.xlsx')
+descriptors_concatenated.to_excel(writer, sheet_name='descriptors', index=False)
+writer.save()
+
+print("Descriptors calculated and stored in {} seconds.".format(time.time()-start_time))
