275efa7ebf49abf0eee423c31af7eb402f317b38
AlexisMolinaMR <alexis.molina@alum.esci.upf.edu>
2020-01-29 16:32:34 +0100

    ML notebook

---
+++ PeleAI/descriptors/xgboost.py
@@ -0,0 +1,8 @@
+from xgboost import XGBClassifier
+import pandas as pdb
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+train = pd.read_csv("train.csv")
+train = pd.read_csv("train.csv")
+train = pd.read_csv("train.csv")
